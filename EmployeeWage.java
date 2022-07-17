4fb33cd32444939ef8503effdf460ccff9a4b534
Akash <akhvnd@gmail.com>
2022-07-17 18:39:42 +0530

    modified:employee Present or Absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,10 +2,22 @@ package com.employeewages.util;
 
 public class EmployeeWage {
 
+	 public void computeAttendance() {
+		 int attendance =(int)(Math.random()*10)%2;
+		 if (attendance == 1 ) {
+	        	
+	        	System.out.println(" Employee is Present ");
+	        	
+	        } else {
+	        	
+	        	System.out.println(" Employee is Absent ");
+	        }
+	 }
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation");
-
+		 EmployeeWage employee = new  EmployeeWage();
+		 employee.computeAttendance();
 	}
 
 }
